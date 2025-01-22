54cab6b98dafb66501d0ff6cf4f3e292bdbc2f48
Shashi Singh <alphasingh8434@gmail.com>
2025-01-22 21:16:34 +0530

    small changes

--- lib/dart_files/home_screen.dart
+++ lib/dart_files/home_screen.dart
@@ -6,9 +6,11 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(
-        'Home Screen',
-        style: TextStyle(fontSize: 24),
+      body: Center(
+        child: Text(
+          'Home Screen',
+          style: TextStyle(fontSize: 24),
+        ),
       ),
     );
   }
