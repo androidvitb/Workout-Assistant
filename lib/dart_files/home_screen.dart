ed3f5ca54e927c087b6ee2424b7fc23e31aeb2ac
Shashi Singh <alphasingh8434@gmail.com>
2025-01-22 21:14:51 +0530

    added a new screen for workout session and a timer on that

--- lib/dart_files/home_screen.dart
+++ lib/dart_files/home_screen.dart
@@ -5,8 +5,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
+    return Scaffold(
+      body: Text(
         'Home Screen',
         style: TextStyle(fontSize: 24),
       ),
